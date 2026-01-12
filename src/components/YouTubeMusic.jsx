b3b74e7dd16955aa5e6de6b09b3d3a8db9615797
Chin Kosal <nangapple82@gmail.com>
2026-01-12 16:42:06 +0700

    fix music

--- src/components/YouTubeMusic.jsx
+++ src/components/YouTubeMusic.jsx
@@ -5,39 +5,67 @@ export default function YouTubeMusic() {
   const iframeRef = useRef(null);
 
   useEffect(() => {
-    const iframe = iframeRef.current;
+    const enableSound = () => {
+      const iframe = iframeRef.current;
+      if (!iframe?.contentWindow) return;
 
-    // Function to unmute on first user interaction
-    const unmuteMusic = () => {
-      if (iframe?.contentWindow) {
+      iframe.contentWindow.postMessage(
+        JSON.stringify({ event: "command", func: "unMute", args: [] }),
+        "*"
+      );
+      iframe.contentWindow.postMessage(
+        JSON.stringify({ event: "command", func: "playVideo", args: [] }),
+        "*"
+      );
+
+      window.removeEventListener("scroll", enableSound);
+      window.removeEventListener("click", enableSound);
+      window.removeEventListener("touchstart", enableSound);
+    };
+
+    const handleVisibilityChange = () => {
+      const iframe = iframeRef.current;
+      if (!iframe?.contentWindow) return;
+
+      if (document.visibilityState === "visible") {
         iframe.contentWindow.postMessage(
-          JSON.stringify({ event: "command", func: "unMute" }),
+          JSON.stringify({ event: "command", func: "seekTo", args: [0, true] }),
+          "*"
+        );
+        iframe.contentWindow.postMessage(
+          JSON.stringify({ event: "command", func: "playVideo", args: [] }),
+          "*"
+        );
+        iframe.contentWindow.postMessage(
+          JSON.stringify({ event: "command", func: "unMute", args: [] }),
           "*"
         );
       }
-
-      window.removeEventListener("click", unmuteMusic);
-      window.removeEventListener("scroll", unmuteMusic);
-      window.removeEventListener("touchstart", unmuteMusic);
     };
 
-    window.addEventListener("click", unmuteMusic);
-    window.addEventListener("scroll", unmuteMusic);
-    window.addEventListener("touchstart", unmuteMusic);
+    window.addEventListener("scroll", enableSound, { once: true });
+    window.addEventListener("click", enableSound, { once: true });
+    window.addEventListener("touchstart", enableSound, { once: true });
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    return () => {
+      window.removeEventListener("scroll", enableSound);
+      window.removeEventListener("click", enableSound);
+      window.removeEventListener("touchstart", enableSound);
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
   }, []);
 
+  const origin = typeof window !== "undefined" ? window.location.origin : "";
+
   return (
-    <div style={{ display: "none" }}>
-      <iframe
-        ref={iframeRef}
-        width="0"
-        height="0"
-        src="https://www.youtube.com/embed/NvIXu1pXNZM?autoplay=1&loop=1&playlist=NvIXu1pXNZM&mute=1&enablejsapi=1"
-        title="YouTube background music"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        allowFullScreen
-      ></iframe>
-    </div>
+    <iframe
+      ref={iframeRef}
+      width="1"
+      height="1"
+      style={{ display: "none" }}
+      src={`https://www.youtube.com/embed/NvIXu1pXNZM?autoplay=1&mute=1&loop=1&playlist=NvIXu1pXNZM&enablejsapi=1&origin=${origin}`}
+      allow="autoplay; encrypted-media"
+    />
   );
 }
